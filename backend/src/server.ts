fb228d35be37226b9814b7adc4ec38291d58434a
jaskaran singh <jaskaran4323@gmail.com>
2026-05-21 12:19:06 -0400

    mysql primsa setup done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,13 @@ import express from 'express';
 const app = express()
 const port = 3000;
 
-const x = 100;
+
 app.get('/', (req, res) => {
-  res.send('Hello World!'+ x);
+  res.send('Hello World!');
   });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
   });
+
+
+  
